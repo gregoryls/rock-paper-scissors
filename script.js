014ee10413fdfe1b436b6850d08cc22159ba1750
gregoryls <greg53x@msn.com>
2022-03-17 16:30:54 -0600

    create function  to decide winner between player and computer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Create a function, computerPlay, that will randomly return 'rock', 'paper', or 'scissors'. 
 // let computerSelection equal the output of computerPlay
-// let playerSelection equal player input of 'rock', 'paper', or 'scissors'
+// let playerSelection equal player input of 'rock', 'paper', or 'scissors'.  Convert all submissions to lowercase. 
 //Function, playRound, that takes playerSelection and computerSelection as inputs and returns the winner. 
 //If player=choice, switch (computer) case rock, case paper, case scissors
 //
@@ -16,3 +16,37 @@ function computerPlay () {
             return 'scissors';        
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'rock'){
+        switch (computerSelection){
+            case 'rock':
+                return 'Tie!';
+            case 'paper':
+                return 'You lose! Paper beats rock';
+            case 'scissors':
+                return 'You win! Rock beats scissors';
+        }
+    }else if (playerSelection === 'paper'){
+        switch (computerSelection){
+            case 'rock':
+                return 'You win! Paper beats rock';
+            case 'paper':
+                return 'Tie!';
+            case 'scissors':
+                return 'You lose! Scissors beats paper';
+        }
+    }else {
+        switch (computerSelection){
+            case 'rock':
+                return 'You lose! Rock beats scissors';
+            case 'paper':
+                return 'You win! Scissors beats paper';
+            case 'scissors':
+                return 'Tie!';
+        }
+    }
+}
+let playerSelection = prompt('Enter rock, paper, or scissors', 'rock')
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
